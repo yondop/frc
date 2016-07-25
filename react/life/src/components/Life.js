f8ddaab2bffd91660f610c66dcddc6cc8f57474a
yondop <yondop@gmail.com>
2016-07-25 16:55:03 +0500

    life init

---
+++ react/life/src/components/Life.js
@@ -0,0 +1,21 @@
+import React, {PropTypes} from 'react';
+
+class Life extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      running: false
+    };
+  }
+
+  render() {
+    return(
+      <div className="">
+
+      </div>
+    );
+  }
+}
+
+export default Life;
